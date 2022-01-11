7739571106ec7d5e0cee6320dde31e754543025a
hassanxelamin <hassanxelamin@gmail.com>
2022-01-10 17:47:54 -0800

    Remove all module files

--- components/Nav/nav.js
+++ components/Nav/nav.js
@@ -1,33 +1,15 @@
 import { navStyles } from '/imports.js';
-// import CartButton from '../CartButton/CartButton.js';
-import { useCart } from '../../hooks/use-cart.js'
-import { cartButtonStyles } from '/imports.js';
+import CartButton from '../CartButton/CartButton.js';
 import Logo from '../Logo/Logo.js';
 import Menu from '../Menu/Menu.js';
-import Link from 'next/link';
 
 const Nav = () => {
-
-    const { totalItems } = useCart();
 
     return (
         <nav className={navStyles.container}>
             <Menu />
             <Logo />
-            <div>
-                <div>
-                    <Link href="/cart">
-                        <a className='flex items-center justify-center text-base text-[15px]'>
-                            <div>
-                                Bag
-                            </div>
-                            <div className={cartButtonStyles.navigationcounter}>
-                                {totalItems}
-                            </div>
-                        </a>
-                    </Link>
-                </div>
-            </div>
+            <CartButton />
         </nav>
     )
 }
