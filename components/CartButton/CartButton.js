90ea877d3cc9584b90777b6802d336ce29e45d1b
hassanxelamin <hassanxelamin@gmail.com>
2022-01-10 17:45:01 -0800

    testing

--- components/CartButton/CartButton.js
+++ components/CartButton/CartButton.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import { useCart } from '../../hooks/use-cart.js'
-import { cartButtonStyles } from '/imports.js';
-import Link from 'next/link';
+// import React from 'react'
+// import { useCart } from '../../hooks/use-cart.js'
+// import { cartButtonStyles } from '/imports.js';
+// import Link from 'next/link';
 
-const CartButton = () => {
+// const CartButton = () => {
 
-    const { totalItems } = useCart();
+//     const { totalItems } = useCart();
 
-    return (
-        <div>
-            <div>
-                <Link href="/cart">
-                    <a className='flex items-center justify-center text-base text-[15px]'>
-                        <div>
-                            Bag
-                        </div>
-                        <div className={cartButtonStyles.navigationcounter}>
-                            {totalItems}
-                        </div>
-                    </a>
-                </Link>
-            </div>
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <div>
+//                 <Link href="/cart">
+//                     <a className='flex items-center justify-center text-base text-[15px]'>
+//                         <div>
+//                             Bag
+//                         </div>
+//                         <div className={cartButtonStyles.navigationcounter}>
+//                             {totalItems}
+//                         </div>
+//                     </a>
+//                 </Link>
+//             </div>
+//         </div>
+//     )
+// }
 
-export default CartButton;
+// export default CartButton;
